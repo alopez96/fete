c4d4908d5f5e2e32f7efa1beb46f861a090cf355
Arturo <alopez96@ucsc.edu>
2018-10-21 22:20:41 -0700

    sprint 1 report

--- app/src/main/java/com/example/arturolopez/fete/AccountPage.java
+++ app/src/main/java/com/example/arturolopez/fete/AccountPage.java
@@ -2,12 +2,21 @@ package com.example.arturolopez.fete;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AccountPage extends AppCompatActivity {
+
+    private TextView nameText;
+    private TextView emailText;
+    private TextView numberText;
+    private TextView bioText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_page);
+
+        //onclick methods to for textViews
     }
 }
